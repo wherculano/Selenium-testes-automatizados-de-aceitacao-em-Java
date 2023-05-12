ceaa8785522cf79e07ec4e505f3abb394bd11b84
Wagner Herculano <wagherculano@hotmail.com>
2023-05-12 16:22:58 -0300

    Configurado projeto para teste inválido de login e removido repeticao de código

--- src/main/java/br/com/alura/leilao/login/LoginTest.java
+++ src/main/java/br/com/alura/leilao/login/LoginTest.java
@@ -1,20 +1,36 @@
 package br.com.alura.leilao.login;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.junit.jupiter.api.Assertions;
 
 public class LoginTest {
+    private WebDriver browser;
+    private static final String URL_LOGIN = "http://localhost:8080/login";
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.setProperty("webdriver.gecko.driver","src/main/java/br/com/alura/leilao/drivers/geckodriver");
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        this.browser = new FirefoxDriver();
+        this.browser.navigate().to(URL_LOGIN);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        this.browser.quit();
+    }
+
     @Test
     public void deveriaEfetuarLoginComDadosValidos() {
-        System.setProperty("webdriver.gecko.driver","src/main/java/br/com/alura/leilao/drivers/geckodriver");
-        WebDriver browser = new FirefoxDriver();
-        browser.navigate().to("http://localhost:8080/login");
-        browser.findElement(By.id("username")).sendKeys("fulano");
-        browser.findElement(By.id("password")).sendKeys("pass");
-        browser.findElement(By.id("login-form")).submit();
+        this.browser.findElement(By.id("username")).sendKeys("fulano");
+        this.browser.findElement(By.id("password")).sendKeys("pass");
+        this.browser.findElement(By.id("login-form")).submit();
 
         try {
             // aguarda meio segundo apenas para dar tempo de efetuar o login
@@ -22,8 +38,26 @@ public class LoginTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Assertions.assertFalse(browser.getCurrentUrl().equals("http://localhost:8080/login"));
-        Assertions.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
-        browser.quit();
+        Assertions.assertFalse(this.browser.getCurrentUrl().equals("http://localhost:8080/login"));
+        Assertions.assertEquals("fulano", this.browser.findElement(By.id("usuario-logado")).getText());
+    }
+
+    @Test
+    public void naoDeveriaEfutuarLoginComDadosInvalidos(){
+        this.browser.findElement(By.id("username")).sendKeys("userQualquer");
+        this.browser.findElement(By.id("password")).sendKeys("senhaInvalida");
+        this.browser.findElement(By.id("login-form")).submit();
+
+        try {
+            // aguarda meio segundo apenas para dar tempo de efetuar o login
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertTrue(this.browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+        Assertions.assertTrue(this.browser.getPageSource().contains("Usuário e senha inválidos"));
+        Assertions.assertThrows(NoSuchElementException.class, () -> this.browser.findElement(By.id("usuario-logado")));
+
     }
 }
